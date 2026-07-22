0cf610b135581669a150ad4b408d69fd7c9037a1
HARI HARAN <hariparamasivan1@gmail.com>
2026-07-22 09:29:04 +0530

    Add files via upload

---
+++ greatest_of_three_numbers.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+if a >= b and a >= c:
+    print("Greatest =", a)
+elif b >= a and b >= c:
+    print("Greatest =", b)
+else:
+    print("Greatest =", c)
